139aa82c80c4db71bc6be2d9d33b1b708757b08d
Eduardo Castro <105288+esgcastro@users.noreply.github.com>
2025-09-22 00:32:28 -0300

    corrige formato do arquivo main.tf

--- main.tf
+++ main.tf
@@ -5,18 +5,18 @@ provider "aws" {
 
 # Chama o módulo da VPC
 module "vpc" {
-  source           = "./modules/vpc"
-  vpc_cidr_block   = "10.0.0.0/16"
-  vpc_name         = "main-vpc"
+  source                   = "./modules/vpc"
+  vpc_cidr_block           = "10.0.0.0/16"
+  vpc_name                 = "main-vpc"
   public_subnet_cidr_block = "10.0.1.0/24"
 }
 
 # Chama o módulo da instância EC2
 module "ec2_instance" {
-  source                     = "./modules/ec2"
-  instance_ami               = "ami-0de716d6197524dd9"
-  instance_type              = "t2.micro"
-  instance_name              = "exemplo-instancia-ec2"
-  subnet_id                  = module.vpc.public_subnet_id
-  vpc_security_group_ids     = [module.vpc.security_group_id]
+  source                 = "./modules/ec2"
+  instance_ami           = "ami-0de716d6197524dd9"
+  instance_type          = "t2.micro"
+  instance_name          = "exemplo-instancia-ec2"
+  subnet_id              = module.vpc.public_subnet_id
+  vpc_security_group_ids = [module.vpc.security_group_id]
 }
