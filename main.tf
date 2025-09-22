b1def5749f55f78a664ebe9835b20a3b5b07d7b5
Eduardo Castro <105288+esgcastro@users.noreply.github.com>
2025-09-22 00:48:41 -0300

    Alterado para forçar um novo workflow do GitHub Actions - tentativa 4

--- main.tf
+++ main.tf
@@ -21,4 +21,4 @@ module "ec2_instance" {
   vpc_security_group_ids = [module.vpc.security_group_id]
 }
 
-# Alteração para forçar um novo workflow pelo GitHub Actions
+# Alteração para forçar um novo workflow pelo GitHub Actions - 2
